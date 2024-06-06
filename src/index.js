e17c20bae72ed18f651e4551b13a7bce73eb9570
KBelltree <98202109+kbelltree@users.noreply.github.com>
2024-06-05 17:09:17 -0700

    Add an import statement, and a eventListener

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import {
   handleSearchEnter,
   handleFirstContentLoad,
   handleTempUnitButtonClick,
+  handleSearchInput,
 } from "./eventHandler.js";
+
+import "./style.css";
 
 const temperatureBtnContainer = document.getElementById(
   "temp-button-container",
@@ -13,4 +16,5 @@ document.addEventListener("DOMContentLoaded", handleFirstContentLoad, {
   once: true,
 });
 searchInput.addEventListener("keydown", handleSearchEnter);
+searchInput.addEventListener("input", handleSearchInput);
 temperatureBtnContainer.addEventListener("click", handleTempUnitButtonClick);
